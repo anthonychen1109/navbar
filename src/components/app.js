160c9a53504b00797198766857f31dc1fb1fffe8
ac065455 <chen.anthony.1109@gmail.com>
2018-01-18 23:04:44 -0500

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// COMPONENTS
+import Navbar from '../containers/navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
